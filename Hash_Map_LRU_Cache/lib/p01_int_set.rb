ab85e1430ab0774a70ccd4a7c906b77e098e55cd
Frankelin Pichardo <kelinp25@gmail.com>
2021-11-01 14:35:56 -0400

    finished class IntSet

--- Hash_Map_LRU_Cache/lib/p01_int_set.rb
+++ Hash_Map_LRU_Cache/lib/p01_int_set.rb
@@ -32,22 +32,29 @@ end
 
 
 class IntSet
+  attr_reader :store, :num_buckets
   def initialize(num_buckets = 20)
     @store = Array.new(num_buckets) { Array.new }
+    @num_buckets = num_buckets
   end
 
   def insert(num)
+    self[num] << num
   end
 
   def remove(num)
+    self[num].delete_if { |el| el == num }
   end
 
   def include?(num)
+    return true if self[num].include?(num)
+    false
   end
 
   private
 
   def [](num)
+    store[num % num_buckets]
     # optional but useful; return the bucket corresponding to `num`
   end
 
