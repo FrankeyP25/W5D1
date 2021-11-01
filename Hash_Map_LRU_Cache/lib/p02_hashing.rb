35f0509ac1864ff76924cc9284a3da5eec2eaa34
Frankelin Pichardo <kelinp25@gmail.com>
2021-11-01 17:02:25 -0400

    added hashing

--- Hash_Map_LRU_Cache/lib/p02_hashing.rb
+++ Hash_Map_LRU_Cache/lib/p02_hashing.rb
@@ -4,11 +4,30 @@ end
 
 class Array
   def hash
+    # new_hash = {}
+    # self.each_with_index do |el,i|
+    #   new_hash[i] = el.hash
+    # end
+    # new_hash
+    arr = self.map do |el|
+      el.hash
+    end
+    arr1 = arr.map.with_index do |el, i|
+      el * i
+    end
+    arr1.sum
   end
 end
 
+
 class String
+  #{a = "22", b = "43"}
   def hash
+    # new_hash = {}
+    # self.each_char.with_index do |ch, i|
+    #   new_hash[ch] = i
+    # end
+    # new_hash
   end
 end
 
